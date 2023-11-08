f5ded9278f86e9a0f5b08af16df69e02a303082f
jiaxu0715 <jiaxu0715@gmail.com>
2023-11-08 11:33:33 +0900

    Initial Commit

---
+++ Speed up walking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Speed up walking
+//
+//  Created by visitor on 2023/11/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Speed up walking/Speed_up_walkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speed_up_walkingApp.swift
+//  Speed up walking
+//
+//  Created by visitor on 2023/11/08.
+//
+
+import SwiftUI
+
+@main
+struct Speed_up_walkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
